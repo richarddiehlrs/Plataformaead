c92072309eb1d3cd2122fe8eba08848e57bcf054
root <thiago.kraetzer@brainweb.com.br>
2021-01-04 16:36:46 -0300

    Fix: fixing sync issue

--- src/pages/PrivatePages/RecordedClasses/index.tsx
+++ src/pages/PrivatePages/RecordedClasses/index.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, {
+  useEffect, useState, useCallback, useRef,
+} from 'react';
 
 import api from 'services/api';
+import { CancelTokenStatic } from 'axios';
 import { convertSecondsToHoursMinutesSeconds } from 'utils/functions';
 import { useAuth } from 'hooks/auth';
 import { SchoolLevel, SchoolLevelSubject, SchoolLevelSubjectSeasonClasses } from 'models/SchoolModels';
@@ -15,63 +18,81 @@ import {
 
 const RecordedClasses: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
   const [schoolLevels, setSchoolLevels] = useState<SchoolLevel[]>([]);
+  const [schoolLevelSubjects, setSchoolLevelSubjects] = useState<SchoolLevelSubject[]>([]);
+  const [schoolLevelSubjectSeasons, setSchoolLevelSubjectSeasons] = useState<SchoolLevelSubject[]>([]);
+  const [videos, setVideos] = useState<SchoolLevelSubjectSeasonClasses[]>([]);
+
   const [selectedSchoolLevel, setSelectedSchoolLevel] = useState({ key: '', value: '' });
   const [selectedSchoolSubject, setSelectedSchoolSubject] = useState({ key: '', value: '' });
 
-  const [schoolLevelSubjects, setSchoolLevelSubjects] = useState<SchoolLevelSubject[]>([]);
-  const [schoolLevelSubjectSeasons, setSchoolLevelSubjectSeasons] = useState<SchoolLevelSubject[]>([]);
-
-  const [videos, setVideos] = useState<SchoolLevelSubjectSeasonClasses[]>([]);
   const [selectedVideoPosition, setSelectedVideoPosition] = useState(0);
 
   const { user } = useAuth();
+
+  const cancelLevelIdReq = useRef<CancelTokenStatic>(null);
+  const cancelSubjectsReq = useRef<CancelTokenStatic>(null);
+  const cancelSubejectSeasonReq = useRef<CancelTokenStatic>(null);
+  const cancelSubjectSeasonInfoReq = useRef<CancelTokenStatic>(null);
 
   const getSchoolLevelSubjectSeasonInfo = useCallback(async (item) => {
     setVideos([]);
     setSelectedVideoPosition(0);
     try {
-      const response = await api.get<SchoolLevelSubjectSeasonClasses[]>(`/school/level/subject/season/class?schoolid=${user.schoolid}&levelid=${selectedSchoolLevel.key}&subjectid=${selectedSchoolSubject.key}&seasonid=${item.key}&userid=${user.userid}`);
+      const response = await api.get<SchoolLevelSubjectSeasonClasses[]>(`/school/level/subject/season/class?schoolid=${user.schoolid}&levelid=${selectedSchoolLevel.key}&subjectid=${selectedSchoolSubject.key}&seasonid=${item.key}&userid=${user.userid}`, {
+        cancelToken: cancelSubjectSeasonInfoReq.current?.source().token,
+      });
       setVideos(response.data);
     } catch (err) {
       console.log(err.message);
     }
-  }, [selectedSchoolLevel, user, selectedSchoolSubject.key]);
+  }, [selectedSchoolLevel, user, selectedSchoolSubject, cancelSubjectSeasonInfoReq]);
 
   const getSchoolLevelSubjectSeason = useCallback(async (item) => {
     setVideos([]);
     setSchoolLevelSubjectSeasons([]);
 
     try {
-      const response = await api.get<SchoolLevelSubject[]>(`/school/level/subject/season?schoolid=${user.schoolid}&levelid=${selectedSchoolLevel.key}&subjectid=${item.key}`);
+      const response = await api.get<SchoolLevelSubject[]>(`/school/level/subject/season?schoolid=${user.schoolid}&levelid=${selectedSchoolLevel.key}&subjectid=${item.key}`, {
+        cancelToken: cancelSubejectSeasonReq.current?.source().token,
+      });
       setSelectedSchoolSubject(item);
       setSchoolLevelSubjectSeasons(response.data);
     } catch (err) {
       console.log(err.message);
     }
-  }, [selectedSchoolLevel, user.schoolid]);
+  }, [selectedSchoolLevel, user.schoolid, cancelSubejectSeasonReq]);
 
   const getSchoolSubjects = useCallback(async (item) => {
+    cancelSubejectSeasonReq.current?.source().cancel('cancelada');
+    cancelSubjectSeasonInfoReq.current?.source().cancel('cancelada');
+
+    console.log(cancelSubjectsReq.current?.source().token);
     setIsLoading(true);
     setVideos([]);
     setSchoolLevelSubjects([]);
     setSchoolLevelSubjectSeasons([]);
 
     try {
-      const response = await api.get<SchoolLevelSubject[]>(`/school/level/subject?schoolid=${user.schoolid}&levelid=${item.key}`);
+      const response = await api.get<SchoolLevelSubject[]>(`/school/level/subject?schoolid=${user.schoolid}&levelid=${item.key}`, {
+        cancelToken: cancelSubjectsReq.current?.source().token,
+      });
       setSelectedSchoolLevel(item);
       setSchoolLevelSubjects(response.data);
     } catch (err) {
       console.log(err.message);
     }
     setIsLoading(false);
-  }, [user.schoolid]);
+  }, [user.schoolid, cancelSubjectSeasonInfoReq, cancelSubejectSeasonReq, cancelSubjectsReq]);
 
   const getSchoolLevels = useCallback(async () => {
     setIsLoading(true);
     try {
-      const response = await api.get<SchoolLevel[]>(`/school/level?schoolid=${user.schoolid}`);
+      const response = await api.get<SchoolLevel[]>(`/school/level?schoolid=${user.schoolid}`, {
+        cancelToken: cancelLevelIdReq.current?.source().token,
+      });
       setSchoolLevels(response.data);
       const mySchoolItem = response.data.find((item) => item.levelid === user.levelid);
       getSchoolSubjects({ key: mySchoolItem?.levelid, value: mySchoolItem?.title });
@@ -79,21 +100,23 @@ const RecordedClasses: React.FC = () => {
       console.log(err.message);
     }
     setIsLoading(false);
-  }, [user, getSchoolSubjects]);
+  }, [user, getSchoolSubjects, cancelLevelIdReq]);
 
   const handlePauseVideo = useCallback(async (info) => {
-    await api.post('/school/level/subject/season/class/user', {
-      classid: videos[selectedVideoPosition].classid,
-      seasonid: videos[selectedVideoPosition].seasonid,
-      levelid: videos[selectedVideoPosition].levelid,
-      subjectid: videos[selectedVideoPosition].subjectid,
-      schoolid: videos[selectedVideoPosition].schoolid,
-      userid: user.userid,
-      videowatched: convertSecondsToHoursMinutesSeconds(info.seconds),
-      videostatus: 'watching',
-      exercisestatus: ' ',
-    });
-  }, [videos, selectedVideoPosition, user.userid]);
+    if (mounted) {
+      await api.post('/school/level/subject/season/class/user', {
+        classid: videos[selectedVideoPosition].classid,
+        seasonid: videos[selectedVideoPosition].seasonid,
+        levelid: videos[selectedVideoPosition].levelid,
+        subjectid: videos[selectedVideoPosition].subjectid,
+        schoolid: videos[selectedVideoPosition].schoolid,
+        userid: user.userid,
+        videowatched: convertSecondsToHoursMinutesSeconds(info.seconds),
+        videostatus: 'watching',
+        exercisestatus: ' ',
+      });
+    }
+  }, [videos, selectedVideoPosition, user.userid, mounted]);
 
   const handleFinishVideo = useCallback((info) => {
     handlePauseVideo(info);
@@ -101,8 +124,12 @@ const RecordedClasses: React.FC = () => {
 
   useEffect(() => {
     setIsLoading(true);
+    setMounted(true);
+
     getSchoolLevels();
     setIsLoading(false);
+
+    return () => setMounted(false);
   }, [user.schoolid, getSchoolLevels]);
 
   return (
